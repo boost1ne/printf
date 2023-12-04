9f421e35124f988640c9547d7ef8ead7779c6c8b
eyassine <leetyacine@gmail.com>
2023-12-05 00:04:57 +0100

    betty style

--- main.h
+++ main.h
@@ -5,12 +5,24 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+/**
+ * struct display - struct contains the derective
+ * and a p to the function associated
+ * @value: the derective
+ * @fp: function pointer
+*/
+
 typedef struct display
 {
-    char value;
-    int (*fp)(va_list p);
+	char value;
+	int (*fp)(va_list p);
 } display_t;
 
 int	_printf(const char *format, ...);
+int _print_c(va_list args);
+int _string_put(va_list p);
+int _puts(char *str);
+int _putchar(char c);
+
 
 #endif
--- printf.c
+++ printf.c
@@ -1,70 +1,101 @@
 #include "main.h"
 
+/**
+ *_putchar - writes character to stdout
+ *@c: the char to be printed
+ *Return: the char
+ ***/
+
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
+
+/**
+ *_puts - writes str to stdout
+ *@str: str
+ *Return: number of printed characters
+ ***/
 
 int _puts(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i]; i++)
-        _putchar(str[i]);
-    return (i);
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	return (i);
 }
 
+/**
+ *_string_put - writes character to stdout
+ *@p: variable arguments list
+ *Return: number of printed characters
+ ***/
 
 int _string_put(va_list p)
 {
-    char *str;
+	char *str;
 
-    str = va_arg(p, char *);
-    if (!str)
-    {
-        _puts("(null)");
-        return (-1);
-    }
-    return (_puts(str));
+	str = va_arg(p, char *);
+	if (!str)
+	{
+		_puts("(null)");
+		return (-1);
+	}
+	return (_puts(str));
 }
+
+/**
+ *_print_c - writes character to stdout
+ *@args: variable arguments list
+ *Return: number of printed characters
+ ***/
 
 int _print_c(va_list args)
 {
-    char c;
+	char c;
 
-    c = va_arg(args, int);
-    return (_putchar(c));
+	c = va_arg(args, int);
+	return (_putchar(c));
 }
+
+/**
+ *_printf - formats and prints data to stdout
+ *@format: format string to check
+ *@...: variable arguments list
+ *
+ *Return: number of printed characters
+ ****/
 
 int	_printf(const char *format, ...)
 {
-    va_list args;
-    unsigned int count = 0;
-    int i;
-    display_t funcs[2] = {
-        {'s', _string_put},
-        {'c', _print_c}
-    };
+	va_list args;
+	unsigned int count = 0;
+	int i;
+	display_t funcs[2] = {
+		{'s', _string_put},
+		{'c', _print_c}
+	};
 
-    if (!format || !*format)
-        return (-1);
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (!*format)
-                return (-1);
-            for (i = 0; i < 2; i++)
-            {
-                if (*format == funcs[i].value)
-                    count += funcs[i].fp(args);
-            }
-        }
-        else
-            count += _putchar(*format);
-        format++;
-    }
-    return (count);
+	if (!format || !*format)
+		return (-1);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (!*format)
+				return (-1);
+			for (i = 0; i < 2; i++)
+			{
+				if (*format == funcs[i].value)
+					count += funcs[i].fp(args);
+			}
+		}
+		else
+			count += _putchar(*format);
+		format++;
+	}
+	return (count);
 }
